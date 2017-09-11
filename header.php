0ad6a88d741e9e72b2ac90b8a5b33fd9189f85fc
Craig Russell <ctr26@cam.ac.uk>
2017-09-12 00:37:20 +0100

    Moved code out of index into footer and header functions (admin bar works now)

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Magdalene MCR Helios</title>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+
+		<link href="<?php echo get_stylesheet_directory_uri()?>/assets/css/main.css" rel="stylesheet"  media="all" type="text/css" />
+
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+
+	</head>
