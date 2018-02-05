23f88170d61c2d43384e7484dec7852fae7b8ec1
Craig Russell <ctr26@cam.ac.uk>
2018-02-05 23:38:10 +0000

    Fixed content displaying I think. Was wrapping with div ids that overwrote the good css.

--- content-page.php
+++ content-page.php
@@ -4,20 +4,21 @@
 */
 ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<div class="entry-content clearfix">
+<!--div id="post-<?php the_ID(); ?>" <?php post_class(); ?>-->
+<h2><?php single_post_title(); ?></h2>
+<p>
+	<!--div--> <!--class="entry-content clearfix"-->
 		<?php
 		the_content();
 		wp_link_pages( array(
-			'before' => '<p class="pages"><strong>' . __( 'Pages:', 'academica' ) . '</strong>',
+			'before' => '<p><strong>' . __( 'Pages:', 'academica' ) . '</strong>',
 			'after' => '</p>',
 			'next_or_number' => 'number'
 		) ); ?>
-	</div><!-- end .entry-content -->
+	<!--/div--><!-- end .entry-content -->
 
 	<div class="entry-meta">
 		<?php edit_post_link( __( 'Edit', 'academica' ), '<span class="edit-link">', '</span>' ); ?>
 	</div><!-- end .entry-meta -->
-
-</div><!-- end #post-## -->
+</p>
+<!--/div--><!-- end #post-## -->
